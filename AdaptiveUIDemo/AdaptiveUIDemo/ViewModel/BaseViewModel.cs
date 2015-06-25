3a401b6c1267bc4c604ca9728be1faad9d4d0357
Sean Veale <wilcravenmckrye@gmail.com>
2015-06-24 21:47:04 -0400

    Add some basics to the demo app

---
+++ AdaptiveUIDemo/AdaptiveUIDemo/ViewModel/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaptiveUIDemo.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+
+    }
+}
