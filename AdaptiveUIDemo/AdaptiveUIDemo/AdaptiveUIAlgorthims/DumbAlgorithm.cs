f2383344b5229990571067dc311b52eb3767c286
Jay Powell <jay.powell@perkinelmer.com>
2015-06-25 09:36:52 -0400

    Added starting interfaces and a couple of basic data structures

---
+++ AdaptiveUIDemo/AdaptiveUIDemo/AdaptiveUIAlgorthims/DumbAlgorithm.cs
@@ -0,0 +1,30 @@
+﻿using AdaptiveUIDemo.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace AdaptiveUIDemo.AdaptiveUIAlgorthims
+{
+    public class DumbAlgorithm : IAlgorithm
+    {
+        public DumbAlgorithm() 
+            : this("Gomer")
+        { }
+
+        public DumbAlgorithm(string name)
+        {
+            AlgorithmName = name;
+        }
+
+        public string AlgorithmName { get; private set; }
+
+        public void Learn(IData dataPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IData> OrderControls()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
