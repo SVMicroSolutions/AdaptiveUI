15ace01fbb3267d3afa3ae2af23c181cb1d736b3
Tom Kinzelman <Tom.Kinzelman@PerkinElmer.com>
2015-06-25 16:58:56 -0400

    Fixes missing use to

--- AdaptiveUIDemo/AdaptiveUIDemo/ViewModel/AdaptiveUIViewModel.cs
+++ AdaptiveUIDemo/AdaptiveUIDemo/ViewModel/AdaptiveUIViewModel.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Input;
 using AdaptiveUIDemo.Interfaces;
-using System.Collections;
+using System.Collections.ObjectModel;
 
 namespace AdaptiveUIDemo.ViewModel
 {
