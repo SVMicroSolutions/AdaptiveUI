0abef7e0fb15c112ed6e72e1c999bfa3f8ac2ed8
Sean <Sean.Veale@PERKINELMER.COM>
2015-06-25 15:29:05 -0400

    Added SeanAlgo class

---
+++ AdaptiveUIDemo/AdaptiveUIDemo/AdaptiveUIAlgorthims/SeanAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using AdaptiveUIDemo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaptiveUIDemo.AdaptiveUIAlgorthims
+{
+    class SeanAlgorithm
+    {
+
+    }
+}
