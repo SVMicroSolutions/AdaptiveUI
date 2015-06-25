2357470ed925e304a96a4e7144a49552e8aacf09
Sean <Sean.Veale@PERKINELMER.COM>
2015-06-25 09:57:03 -0400

    Adding a basic IDataPersistanceUI and and databing example

--- AdaptiveUIDemo/AdaptiveUIDemo/ViewModel/AdaptiveUIViewModel.cs
+++ AdaptiveUIDemo/AdaptiveUIDemo/ViewModel/AdaptiveUIViewModel.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace AdaptiveUIDemo.ViewModel
 {
-    public class AdaptiveUIViewModel :BaseViewModel
+    public class AdaptiveUIViewModel : BaseViewModel
     {
+        #region Properties 
+        private string appName = String.Empty;
+
+        public string AppName
+        {
+            get
+            {
+                return this.appName;
+            }
+            set
+            {
+                if (value != this.appName)
+                {
+                    this.appName = value;
+                    RaisePropertyChanged();
+                }
+
+            }
+        }
+        #endregion
+
+        public AdaptiveUIViewModel()
+        {
+            this.AppName = "Adaptive UI Rocks!";
+
+        }
 
     }
 }
