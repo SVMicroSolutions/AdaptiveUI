683ef3bd812d0c797567f4215bae45acf2822035
Tom Kinzelman <Tom.Kinzelman@PerkinElmer.com>
2015-06-26 09:15:13 -0400

    local commit of algorithm

---
+++ AdaptiveUIDemo/AdaptiveUIDemo/AdaptiveUIAlgorthims/TomAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdaptiveUIDemo.Interfaces;
+using System.Collections;
+
+namespace AdaptiveUIDemo.AdaptiveUIAlgorthims
+{
+    /// <summary>
+    /// Implements a sigmoid function to impose lower and upper bounds on a control's rank.
+    /// </summary>
+    public class TomAlgorithm : DumbAlgorithm
+    {
+        public TomAlgorithm()
+            : base("Europa")
+        {}
+
+        public TomAlgorithm(string name)
+            : base(name)
+        {}
+
+		public override List<IData> OrderControls()
+		{
+			Dictionary<IData, double> originalList = HitCountData;
+			Dictionary<IData, double> listToReturn = new Dictionary<IData, double>();
+
+			while (originalList.Count > 0)
+			{
+				double dta = originalList.ind;
+				int indexToMax = 0;
+				for (int index = 0; index < originalList.Count; index++)
+				{
+					
+					if (originalLis[indexToMax]. > indexToMax)
+						indexToMax = index;
+
+				}
+		}
+	}
+}
