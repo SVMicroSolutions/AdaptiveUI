a0d9ca5427ce74791798f7dc19c7a64cddc965a7
Tom Kinzelman <Tom.Kinzelman@PerkinElmer.com>
2015-06-26 10:33:38 -0400

    Resharper cleanup

--- AdaptiveUIDemo/AdaptiveUIDemo/AdaptiveUIAlgorthims/TomAlgorithm.cs
+++ AdaptiveUIDemo/AdaptiveUIDemo/AdaptiveUIAlgorthims/TomAlgorithm.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using AdaptiveUIDemo.Interfaces;
-using System.Collections;
 
 namespace AdaptiveUIDemo.AdaptiveUIAlgorthims
 {
@@ -13,36 +11,33 @@ namespace AdaptiveUIDemo.AdaptiveUIAlgorthims
 	{
 		public TomAlgorithm()
 			: base("Europa")
-		{ }
+		{
+		}
 
 		public TomAlgorithm(string name)
 			: base(name)
-		{ }
+		{
+		}
 
 		public override List<IData> OrderControls()
 		{
-			Dictionary<IData, double> hitListCopy = new Dictionary<IData, double>();
-			List<IData> listToReturn = new List<IData>();
+			var listToReturn = new List<IData>();
 
 			// make deep copy of HitCountData so we do not modify the original values.
-			foreach (KeyValuePair<IData, double> item in HitCountData)
-			{
-				hitListCopy.Add(item.Key, item.Value);
-			}
+			var hitListCopy = HitCountData.ToDictionary(item => item.Key, item => item.Value);
 
 			while (hitListCopy.Count > 0)
 			{
-				int indexToMax = 0;
+				var indexToMax = 0;
 				double max = 0;
 
-				for (int index = 0; index < hitListCopy.Count; index++)
+				for (var index = 0; index < hitListCopy.Count; index++)
 				{
 					var value = hitListCopy.ElementAt(index).Value;
-					if (value > max)
-					{
-						max = value;
-						indexToMax = index;
-					}
+					if (!(value > max))
+						continue;
+					max = value;
+					indexToMax = index;
 				}
 				var item = hitListCopy.ElementAt(indexToMax);
 				hitListCopy.Remove(item.Key);
